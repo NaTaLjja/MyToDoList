76926bf53000d03b5a98724a74bafcb8e522fdfd
Natalja <natali.dorogaya@gmail.com>
2023-09-25 17:23:41 +0200

    Added layout files

---
+++ app/src/main/java/com/example/hw_db/ListFragment.kt
@@ -0,0 +1,24 @@
+package com.example.hw_db
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ListFragment:Fragment() {
+    private lateinit var viewModel: TaskViewModel
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.list_fragment, container, false)
+    }
+}
